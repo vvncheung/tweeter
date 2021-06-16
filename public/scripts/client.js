3214bd4016ec1b2ee5c3c72ba588a9620d5bbe10
Vivian Cheung <hi@viviancheung.ca>
2021-06-16 21:21:26 +0000

    fetch tweets with ajax, refetch on submission, form validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,74 +4,86 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const renderTweets = function(tweets) {
+  $('.tweets-container').empty();
+  tweets.forEach(tweet => {
+    const $tweet = createTweetElement(tweet);
+    $('.tweets-container').prepend($tweet);
+  });
+};
+
+const createTweetElement = function(tweet) {
+  let html = `
+  <article class="tweet">
+  <header>
+    <div class="user">
+      <img src="${tweet.user.avatars}" class="profile-pic"/> 
+      <h1 id="name">${tweet.user.name}</h1>
+    </div>
+    <h1 id="username">${tweet.user.handle}</h1>
+  </header>
+  
+  <div class="tweet-body">
+    <p>${tweet.content.text}</p>
+  </div>
+  
+  <footer>
+    <p>
+      ${timeago.format(tweet.created_at)}
+    </p>
+    <div>
+      <i class="fab fa-font-awesome-flag" id="flag"></i> 
+      <i class="fa fa-retweet" id="retweet"></i>  
+      <i class="fa fa-heart" id="heart"></i>  
+    </div>
+  </footer>
+  </article>
+      `;
+  return html;
+};
+
+
+const loadTweets = function() {
+  $.ajax('/tweets', { method: 'GET' })
+    .done((data) => {
+      console.log("success");
+      renderTweets(data);
+    })
+    .fail(() => {
+      console.log("fail");
+    });
+};
+
 $(document).ready(()=> {
 
-  const renderTweets = function(tweets) {
-    tweets.forEach(tweet => {
-      const $tweet = createTweetElement(tweet);
-      $('.tweets-container').append($tweet);
-    });
-  };
+  const $button = $(".submit-button");
+  loadTweets();
 
-  const createTweetElement = function(tweet) {
-    let html = `
-    <article class="tweet">
-    <header>
-      <div class="user">
-        <img src="${tweet.user.avatars}" class="profile-pic"/> 
-        <h1 id="name">${tweet.user.name}</h1>
-      </div>
-      <h1 id="username">${tweet.user.handle}</h1>
-    </header>
-    
-    <div class="tweet-body">
-      <p>${tweet.content.text}</p>
-    </div>
-    
-    <footer>
-      <p>
-        ${timeago.format(tweet.created_at)}
-      </p>
-      <div>
-        <i class="fab fa-font-awesome-flag" id="flag"></i> 
-        <i class="fa fa-retweet" id="retweet"></i>  
-        <i class="fa fa-heart" id="heart"></i>  
-      </div>
-    </footer>
-    </article>
-        `;
-    return html;
-  };
+  $('form').on("submit", function(event) {
+    event.preventDefault();
+    const serializedData = $('#tweet-text').serialize();
+    let params = {
+      method: 'POST',
+      url: 'http://localhost:8080/tweets',
+      data: serializedData
+    };
 
-  renderTweets(data);
+    if ($('textarea').val() === "" || $('textarea').val() == null) {
+      alert("There is no tweet to be tweeted.");
+    } else if ($('textarea').val().length >= 140) {
+      alert("Your tweet is too long.");
+    } else {
+      $.ajax(params)
+        .done(() => {
+          console.log("success");
+          $('textarea').val("");
+          loadTweets();
+        })
+        .fail(() => {
+          console.log("fail");
+        });
+    }
+  });
 });
 
-$( ".submit-button" ).submit(function( event ) {
-  alert( "Handler for .submit() called." );
-  event.preventDefault();
-});
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
